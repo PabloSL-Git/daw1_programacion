6f2a5fcdc6fbfe83646fb16140d5dcd37c79a3f8
PabloSL-Git <santamarialoredo@gmail.com>
2026-01-14 09:58:15 +0100

    sql echo 1 y 2 14/1/26

--- MySQL/Unidad 3 act1.sql
+++ MySQL/Unidad 3 act1.sql
@@ -1,26 +1,54 @@
--- 1
+-- 1 Obtener todos los datos de todos los empleados.
+
 select *
 from empleados;
 
--- 2
+-- 2 Obtener la extensión telefónica de “Juan López”.
+
 select *
 from empleados
 where nomem = 'Juan' and ape1em = 'Lopez' ;
 
--- 3
+-- 3 Obtener el nombre completo de los empleados que tienen más de un hijo.
+
 select nomem, ape1em, ape2em
 from empleados
 where numhiem > 0;
 
--- 4
+-- 4 Obtener el nombre completo y en una sola columna de los empleados que tienen entre 1 y 3 hijos.
+
 select CONCAT_WS(' ', nomem, ape1em, ape2em) as nombreCompleto
 from empleados
 where numhiem between 1 and 3;
 
--- 5
+-- 5 Obtener el nombre completo y en una sola columna de los empleados sin comisión.
 
+select CONCAT_WS(' ', nomem, ape1em, ape2em) as nombreCompleto
+from empleados
+where comisem is null ;
 
--- 9
+-- 6 Obtener la dirección del centro de trabajo “Sede Central”.
+
+select dirce
+from centros
+where nomce = ' SEDE CENTRAL';
+
+-- 7 Obtener el nombre de los departamentos que tienen más de 6000 € de presupuesto.
+
+select nomde
+from departamentos
+where presude > 6000;
+
+-- 8 Obtener el nombre de los departamentos que tienen de presupuesto 6000 € o más.
+ 
+ select nomde
+from departamentos
+where presude >= 6000;
+
+-- 9 Obtener el nombre completo y en una sola columna de los empleados que llevan trabajando
+-- en nuestra empresa más de 1 año. 
+-- (Añade filas nuevas para poder comprobar que tu consulta funciona).
+
 select nomem, ape1em, ape2em,
       -- CONCAT(nomem, ' ', ape1em, ' ', IFNULL(ape2em, '')),
        CONCAT_WS(' ', nomem, ape1em, ape2em) AS nombreCompleto
@@ -28,7 +56,10 @@ from empleados
 where fecinem <= '2025/1/12' 
 order by fecinem;
 
--- 10
+-- 10 Obtener el nombre completo y en una sola columna de los empleados que llevan
+-- trabajando en nuestra empresa entre 1 y tres años.
+-- (Añade filas nuevas para poder comprobar que tu consulta funciona).
+
 select concat(nomem,' ',
 	          ape1em,' ',
               ifnull(ape2em,' ')) as nombreCompleto
--- MySQL/Unidad 3 act2.sql
+++ MySQL/Unidad 3 act2.sql
@@ -45,7 +45,7 @@ where nomde = 'PERSONAL' or 'FINANZAS';
 
 -- 4 Obtener el nombre del director actual del departamento “Personal”.
 
-select nomem
+SELECT nomem
 FROM centros JOIN departamentos
 	USING (numce)
 JOIN dirigir
@@ -53,6 +53,37 @@ JOIN dirigir
 JOIN empleados
 	ON empleados.numem = dirigir.numempdirec
 where nomde = 'PERSONAL';
+
+-- 5 Obtener el nombre y el presupuesto de los departamentos que están ubicados en la “SEDE CENTRAL”.
+
+select numde, presude, nomce
+from  departamentos join centros
+	using (numce)
+where nomce = ' SEDE CENTRAL'; -- fallava por que en la base de datos tenia un espacio al principio
+
+-- 6 Obtener el nombre de los centros de trabajo cuyo presupuesto esté entre 100000 € y 150000 €.
+
+select numce
+from centros join departamentos
+	using (numce)
+where presude between 100000 and 150000;
+
+-- 7 Obtener las extensiones telefónicas del departamento “Finanzas”. No deben salir extensiones repetidas.
+
+select DISTINCT extelem
+from departamentos join empleados
+	using (numde)
+where nomde = 'FINANZAS';
+
+-- 8 Obtener el nombre completo y en una sola columna de todos los directores que ha tenido un departamento cualquiera.
+
+SELECT concat_ws(' ', nomem, ape1em, ape2em) as nombreCompleto
+from empleados join departamentos
+	using (numde)
+join dirigir
+	ON empleados.numem = dirigir.numempdirec
+;
+
 
 
 
