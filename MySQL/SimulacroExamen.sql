d5b4df2df2d6c44eba4fb39b4a3e450929e26adb
PabloSL-Git <santamarialoredo@gmail.com>
2026-02-01 19:06:57 +0100

    termina simulacro sql 1/2/26

--- MySQL/SimulacroExamen.sql
+++ MySQL/SimulacroExamen.sql
@@ -74,6 +74,7 @@ end $$
 delimiter ;
 call simulacroP5("jazmin");
 
+
 -- P6 Si el preciobase de la casa es el precio del alquiler de cada día,
 -- prepara una rutina que permita ver el valor medio de lo que ha costado
 -- cada reserva (ten en cuenta que tenemos el número de días de cada estancia en las reservas).
@@ -113,10 +114,90 @@ begin
 where reservas.codreserva = codigoReserva;
 end $$
 delimiter ;
-call simulacroP7(2, @nombre, @telefono_correo);
+call simulacroP7(1, @nombre, @telefono_correo);
 select @nombre, @telefono_correo;
 
--- P8
+-- P8 Prepara una rutina que muestre el número de reservas que se han 
+-- hecho este año en cada zona (usa la fecha de la reserva y la funcion year donde: 
+-- year(miFecha): miFecha es un date ⇒ la función year devuelve un valor entero (el año de miFecha)
+
+-- version procedure
+DELIMITER $$
+DROP PROCEDURE IF EXISTS simulacroP8a;
+$$
+CREATE PROCEDURE simulacroP8(fechaUsuario DATE)
+BEGIN
+    SELECT *
+    FROM reservas
+    WHERE YEAR(fecreserva) = YEAR(fechaUsuario);
+END
+$$
+DELIMITER ;
+CALL simulacroP8('2012-04-02');
+
+-- version function
+delimiter $$
+drop function if exists simulacroP8b;
+$$
+create function simulacroP8b(fechaUsuario date)
+returns int(30)
+deterministic
+begin
+	declare reserva int(30);
+    
+	select COUNT(*) into reserva
+    from reservas
+    WHERE YEAR(reservas.fecreserva) = YEAR(fechaUsuario);
+return reserva;
+end $$
+delimiter ;
+select simulacroP8b('2012-04-02');
+
+-- P9 Prepara una rutina que, dado un código de zona,
+-- muestre el número de reservas que se han hecho este año en dicha zona.
+
+delimiter $$
+drop procedure if exists simulacroP9;
+$$
+create procedure simulacroP9 (codzona int(20))
+begin
+	select reservas.codreserva as reservas
+    from casas join reservas
+    using(codcasa)
+    where year(reservas.fecreserva) = YEAR(CURDATE());
+end $$
+delimiter ;
+call simulacroP9(1);
+
+-- P10 Queremos saber cual es la media de los metros cuadrados que tienen las casas de nuestra aplicación,
+-- el máximo número de baños y de habitaciones.
+
+SELECT 
+    AVG(m2) AS mediaMetrosCuadrados,
+    MAX(numbanios) AS maxBaños,
+    MAX(numhabit) AS maxHabitaciones
+FROM casas;
+
+/* 
+FUNCIONES DE AGREGACIÓN (EXAMEN)
+
+Sirven para resumir varias filas en un solo resultado.
+
+FUNCIONES IMPORTANTES:
+- COUNT(*)     -> Cuenta todas las filas
+- COUNT(campo) -> Cuenta filas NO NULL
+- SUM(campo)   -> Suma valores
+- AVG(campo)   -> Media (ignora NULL)
+- MAX(campo)   -> Valor máximo
+- MIN(campo)   -> Valor mínimo
+
+
+- AVG y SUM ignoran valores NULL
+- COUNT(*) cuenta incluso filas con NULL
+- COUNT(campo) NO cuenta NULL
+- MAX y MIN funcionan aunque haya NULL
+- Se suelen usar con GROUP BY
+*/
 
 
 
