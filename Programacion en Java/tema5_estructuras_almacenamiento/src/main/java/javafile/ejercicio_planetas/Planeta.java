11f0a5bce7fa0df1fdbf541ed311b6398907bfb7
PabloSL-Git <santamarialoredo@gmail.com>
2026-02-16 12:44:53 +0100

    comienza 3

--- Programacion en Java/tema5_estructuras_almacenamiento/src/main/java/javafile/ejercicio_planetas/Planeta.java
+++ Programacion en Java/tema5_estructuras_almacenamiento/src/main/java/javafile/ejercicio_planetas/Planeta.java
@@ -3,18 +3,26 @@ package javafile.ejercicio_planetas;
 public class Planeta {
 
     // enum los palnetas sistema, cada distancia al sol final,
-    //  nombre, clase sistema solar lista planetas
+    // nombre, clase sistema solar lista planetas
 
     private PlanetasPermitidos planetas;
     private final double distancia;
-    
+
     public Planeta(PlanetasPermitidos planetas, double distancia) {
         this.planetas = planetas;
         this.distancia = distancia;
     }
 
-    
+    public PlanetasPermitidos getPlanetas() {
+        return planetas;
+    }
 
+    public void setPlanetas(PlanetasPermitidos planetas) {
+        this.planetas = planetas;
+    }
 
+    public double getDistancia() {
+        return distancia;
+    }
 
 }
