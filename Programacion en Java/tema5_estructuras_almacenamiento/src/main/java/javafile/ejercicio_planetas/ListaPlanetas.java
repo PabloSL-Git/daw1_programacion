9051766cc48626949b7b78a2c8f5c2e39b67fc0a
PabloSL-Git <santamarialoredo@gmail.com>
2026-02-17 09:12:08 +0100

    prueva netbeans

--- Programacion en Java/tema5_estructuras_almacenamiento/src/main/java/javafile/ejercicio_planetas/ListaPlanetas.java
+++ Programacion en Java/tema5_estructuras_almacenamiento/src/main/java/javafile/ejercicio_planetas/ListaPlanetas.java
@@ -8,17 +8,15 @@ public class ListaPlanetas {
     private List<Planeta> listaPlanetas;
 
     public ListaPlanetas() {
-    listaPlanetas = new ArrayList<>();
+        listaPlanetas = new ArrayList<>();
 
-    listaPlanetas.add(new Planeta(PlanetasPermitidos.MERCURIO, 57.9));
-    listaPlanetas.add(new Planeta(PlanetasPermitidos.VENUS, 108.2));
-    listaPlanetas.add(new Planeta(PlanetasPermitidos.TIERRA, 149.6));
-    listaPlanetas.add(new Planeta(PlanetasPermitidos.MARTE, 227.9));
-    listaPlanetas.add(new Planeta(PlanetasPermitidos.JUPITER, 778.5));
-    listaPlanetas.add(new Planeta(PlanetasPermitidos.SATURNO, 1434.0));
-    listaPlanetas.add(new Planeta(PlanetasPermitidos.URANO, 2871.0));
-    listaPlanetas.add(new Planeta(PlanetasPermitidos.NEPTUNO, 4495.1));
-}
-
-
+        listaPlanetas.add(new Planeta(PlanetasPermitidos.MERCURIO, 57.9));
+        listaPlanetas.add(new Planeta(PlanetasPermitidos.VENUS, 108.2));
+        listaPlanetas.add(new Planeta(PlanetasPermitidos.TIERRA, 149.6));
+        listaPlanetas.add(new Planeta(PlanetasPermitidos.MARTE, 227.9));
+        listaPlanetas.add(new Planeta(PlanetasPermitidos.JUPITER, 778.5));
+        listaPlanetas.add(new Planeta(PlanetasPermitidos.SATURNO, 1434.0));
+        listaPlanetas.add(new Planeta(PlanetasPermitidos.URANO, 2871.0));
+        listaPlanetas.add(new Planeta(PlanetasPermitidos.NEPTUNO, 4495.1));
+    }
 }
