9d838724b100c38e55d79e8b21b55aa48dedd840
PabloSL-Git <santamarialoredo@gmail.com>
2026-03-12 12:35:04 +0100

    arregla enum tarea6b

--- Programacion en Java/tema6_avanzados_poo/src/main/java/herencias/tarea6b/Especialidad.java
+++ Programacion en Java/tema6_avanzados_poo/src/main/java/herencias/tarea6b/Especialidad.java
@@ -5,32 +5,20 @@ public enum Especialidad {
     Secundaria("Secundaria", 0.25),
     ProfesorTecnico("PT", 0.23);
 
-    private String nombre;
-    private double irpf;
+    private final String nombre;
+    private final double irpf;
 
     Especialidad(String nombre, double irpf) {
         this.nombre = nombre;
         this.irpf = irpf;
     }
 
-    Especialidad() {
-
-    }
-
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public double getIrpf() {
         return irpf;
-    }
-
-    public void setIrpf(double irpf) {
-        this.irpf = irpf;
     }
 
 }
