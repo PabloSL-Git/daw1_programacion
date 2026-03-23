8325325bec788bdaefaae342c7f99c138646e087
PabloSL-Git <santamarialoredo@gmail.com>
2026-03-23 10:30:41 +0100

    termina calculadora

--- Programacion en Java/tema6_avanzados_poo/src/main/java/herencias/calculadora/PanelPrincipal.java
+++ Programacion en Java/tema6_avanzados_poo/src/main/java/herencias/calculadora/PanelPrincipal.java
@@ -4,41 +4,106 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-/**
- * @author Juan Carlos Fernández Vico
- */
+public class PanelPrincipal extends JPanel implements ActionListener {
 
-public class PanelPrincipal extends JPanel {
-
-	// Atributos de la clase (privados)
 	private PanelBotones botonera;
 	private JTextArea areaTexto;
-	private int tipoOperacion;
 
-	// Constructor
+	private double num1 = 0;
+	private double num2 = 0;
+	private int tipoOperacion = -1; // 1:+ 2:- 3:* 4:/
+
 	public PanelPrincipal() {
 		initComponents();
-		tipoOperacion = -1; // No hay operaciones en la calculadora
 	}
 
-	// Se inicializan los componentes gráficos y se colocan en el panel
 	private void initComponents() {
-		// Creamos el panel de botones
+
 		botonera = new PanelBotones();
-		// Creamos el área de texto
-		areaTexto = new JTextArea(10, 50);
+
+		areaTexto = new JTextArea(2, 20);
 		areaTexto.setEditable(false);
 		areaTexto.setBackground(Color.white);
 
-		// Establecemos layout del panel principal
 		this.setLayout(new BorderLayout());
-		// Colocamos la botonera y el área texto
 		this.add(areaTexto, BorderLayout.NORTH);
 		this.add(botonera, BorderLayout.SOUTH);
 
+		// Añadir listeners a los botones
+		for (JButton boton : botonera.getgrupoBotones()) {
+			boton.addActionListener(this);
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		String texto = ((JButton) e.getSource()).getText();
+
+		// Si es numero
+		if (texto.matches("[0-9]")) {
+			areaTexto.append(texto);
+		}
+
+		// Operaciones
+		else if (texto.equals("+")) {
+			num1 = Double.parseDouble(areaTexto.getText());
+			tipoOperacion = 1;
+			areaTexto.setText("");
+		}
+
+		else if (texto.equals("-")) {
+			num1 = Double.parseDouble(areaTexto.getText());
+			tipoOperacion = 2;
+			areaTexto.setText("");
+		}
+
+		else if (texto.equals("*")) {
+			num1 = Double.parseDouble(areaTexto.getText());
+			tipoOperacion = 3;
+			areaTexto.setText("");
+		}
+
+		else if (texto.equals("/")) {
+			num1 = Double.parseDouble(areaTexto.getText());
+			tipoOperacion = 4;
+			areaTexto.setText("");
+		}
+
+		// Resultado
+		else if (texto.equals("=")) {
+			num2 = Double.parseDouble(areaTexto.getText());
+			double resultado = 0;
+
+			switch (tipoOperacion) {
+				case 1:
+					resultado = num1 + num2;
+					break;
+				case 2:
+					resultado = num1 - num2;
+					break;
+				case 3:
+					resultado = num1 * num2;
+					break;
+				case 4:
+					resultado = num1 / num2;
+					break;
+			}
+
+			areaTexto.setText(String.valueOf(resultado));
+		}
+
+		// Limpiar
+		else if (texto.equals("C")) {
+			areaTexto.setText("");
+			num1 = 0;
+			num2 = 0;
+			tipoOperacion = -1;
+		}
 	}
 }
