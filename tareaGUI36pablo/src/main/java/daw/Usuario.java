22a5116ed686df8f6423d8134324595edaadc0d1
PabloSL-Git <santamarialoredo@gmail.com>
2026-03-25 17:48:15 +0100

    tarea GUI36

---
+++ tareaGUI36pablo/src/main/java/daw/Usuario.java
@@ -0,0 +1,47 @@
+package daw;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author pablo
+ */
+import java.util.Objects;
+
+public class Usuario {
+    private String usuario;
+    private String contrasenia;
+
+    public Usuario(String usuario, String contrasenia) {
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contraseña) {
+        this.contrasenia = contraseña;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return this.usuario.equals(usuario.usuario);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(usuario);
+    }
+}
