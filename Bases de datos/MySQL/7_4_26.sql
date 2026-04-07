7ab308a67536922e314e699855179c2c86e35267
PabloSL-Git <santamarialoredo@gmail.com>
2026-04-07 10:57:34 +0200

    edit bd

--- Bases de datos/MySQL/7_4_26.sql
+++ Bases de datos/MySQL/7_4_26.sql
@@ -6,11 +6,10 @@ CREATE TRIGGER venta
 BEFORE UPDATE ON pedidos
 FOR EACH ROW
 BEGIN
-    DECLARE stock_actual INT;
-
     -- Obtener el stock del producto
-    SELECT stock INTO stock_actual
-    FROM productos;
+    SELECT stock --  INTO stock_actual
+    FROM productos
+    where codproducto = 
 
 
 END $$
