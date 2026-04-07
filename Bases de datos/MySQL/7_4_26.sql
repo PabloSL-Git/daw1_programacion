0512abb4ab7189672e8df2345b23b0733a5d26a3
PabloSL-Git <santamarialoredo@gmail.com>
2026-04-07 10:09:30 +0200

    antes examen

---
+++ Bases de datos/MySQL/7_4_26.sql
@@ -0,0 +1,18 @@
+-- alguien intenta hacer venta de stock y si no hay que so se haga, hacer con trigger
+
+DELIMITER $$
+
+CREATE TRIGGER venta
+BEFORE UPDATE ON pedidos
+FOR EACH ROW
+BEGIN
+    DECLARE stock_actual INT;
+
+    -- Obtener el stock del producto
+    SELECT stock INTO stock_actual
+    FROM productos;
+
+    END IF;
+END $$
+
+DELIMITER ;
