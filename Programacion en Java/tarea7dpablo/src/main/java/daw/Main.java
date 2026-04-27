fad5763e853a467871a7ce0519b3fb186629c819
PabloSL-Git <santamarialoredo@gmail.com>
2026-04-27 10:15:35 +0200

    casi termina 7d

--- Programacion en Java/tarea7dpablo/src/main/java/daw/Main.java
+++ Programacion en Java/tarea7dpablo/src/main/java/daw/Main.java
@@ -3,7 +3,9 @@ package daw;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,17 +13,18 @@ public class Main {
         ObjectMapper mapper = new ObjectMapper();
 
         try {
+
             // Leer desde resources
             InputStream is = Main.class.getClassLoader()
-                    .getResourceAsStream("precipatacionesBadajoz.json");
+                    .getResourceAsStream("precipitacionesBadajoz.json");
 
-            // Convertir a array
+            // Convertir JSON a array de objetos
             Precipitacion[] array = mapper.readValue(is, Precipitacion[].class);
 
-            // Pasar a lista
+            // Pasar array a lista
             List<Precipitacion> lista = Arrays.asList(array);
 
-            // Mostrar
+            // Mostrar lista por consola
             for (Precipitacion p : lista) {
                 System.out.println(
                         p.getEstacionMeteorologica() + " - " +
@@ -29,8 +32,61 @@ public class Main {
                                 p.getFecha());
             }
 
+            // Crear mapa estación a precipitación
+            Map<String, Float> mapa = new HashMap<>();
+
+            for (Precipitacion p : lista) {
+                mapa.put(p.getEstacionMeteorologica(), p.getPrecipitacion());
+            }
+
+            // Mostrar el map por consola
+            for (Map.Entry<String, Float> entry : mapa.entrySet()) {
+                System.out.println(entry.getKey() + " -> " + entry.getValue());
+            }
+
+            // Guardar el map en un fichero JSON
+            mapper.writeValue(
+                    new java.io.File("mapaPrecipitaciones.json"),
+                    mapa);
+
+            System.out.println("Mapa guardado en JSON correctamente");
+
+            // Registro con mayor precipitación
+            Precipitacion max = lista.stream()
+                    .max((p1, p2) -> Float.compare(p1.getPrecipitacion(), p2.getPrecipitacion()))
+                    .orElse(null);
+
+            // Mostrar mayor precipitacion
+            System.out.println("MAYOR PRECIPITACION");
+            System.out.println(
+                    max.getEstacionMeteorologica() + " - " +
+                            max.getPrecipitacion() + " mm - " +
+                            max.getFecha());
+
+            // mostrar estaciones
+            long contador = lista.stream()
+                    .filter(p -> p.getFecha().get(1) == 10) // octubre
+                    .filter(p -> p.getFecha().get(2) >= 10 && p.getFecha().get(2) <= 20)
+                    .count();
+
+            System.out.println("ESTACIONES ENTRE 10 Y 20 DE OCTUBRE");
+            System.out.println(contador);
+
+            // mostrar media precipationes
+
+            double media = lista.stream()
+                    .filter(p -> p.getFecha().get(1) == 10) // octubre
+                    .filter(p -> p.getFecha().get(2) >= 10 && p.getFecha().get(2) <= 20)
+                    .mapToDouble(Precipitacion::getPrecipitacion)
+                    .average()
+                    .orElse(0);
+
+            System.out.println("MEDIA PRECIPITACIONES (10-20 OCTUBRE)");
+            System.out.println(media);
+
         } catch (Exception e) {
-            throw new RuntimeException("Erroro al leer json", e);
+            throw new RuntimeException("Error al leer json", e);
         }
+
     }
 }
