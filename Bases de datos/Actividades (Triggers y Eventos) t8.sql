2ab54366f91c1291f25c370de2300ad3082bf422
PabloSL-Git <santamarialoredo@gmail.com>
2026-04-28 14:26:03 +0200

    continua

--- Bases de datos/Actividades (Triggers y Eventos) t8.sql
+++ Bases de datos/Actividades (Triggers y Eventos) t8.sql
@@ -1,9 +1,14 @@
 -- Unidad 8. Elementos avanzados de BBDD's relacionales Actividades (Triggers y Eventos)
 -- Para la base de datos empresaclase haz los siguientes ejercicios:
 
+-- ver triggers
+show triggers from empresaclase;
+
+
 -- 1. Comprueba que no podamos contratar a empleados que no tengan 16 años.
 
 use empresaclase;
+
 delimiter $$
 drop trigger if exists compruebaEdad;
 create trigger compruebaEdad
@@ -17,9 +22,30 @@ begin
 end$$
 delimiter ;
 
--- ver triggers
-show triggers from empresaclase;
-
 -- comprobar trigger
 insert into empleados (numem, nomem, fecnaem, fecinem)
 values (21, 'holis', '2025-02-17', '2025-02-17');
+
+-- 2 Comprueba que el departamento de las personas que ejercen la dirección de los departamentos pertenezcan a dicho departamento.
+
+use empresaclase;
+delimiter $$
+drop trigger if exists compruebaDepartamentoPersonas;
+create trigger compruebaDepartamentoPersonas
+before insert on dirigir
+for each row
+begin 
+IF (select numde
+	from empleados
+    where numem = new.numempdirec)
+    <> new.numdepto then
+    
+	SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'director debe pertenezar al departamento que dirige';
+    end if;
+end $$
+delimiter ;
+
+-- comprobar trigger
+insert into dirigir (numdepto, numempdirec, fecinidir, fecfindir, tipodir)
+values (112, 500, '2003-08-03', null, '1');
